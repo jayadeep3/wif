92b4811610a98fad01dc02a1983a51032d3424a7
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2024-12-23 16:05:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my-bucket" {
   name          = "bkt-demo-000"
-  location      = "us-central1"
+  location      = "asia-south1"
   project = "tt-dev-001"
   force_destroy = true
   public_access_prevention = "enforced"
@@ -8,7 +8,7 @@ resource "google_storage_bucket" "my-bucket" {
 
 resource "google_storage_bucket" "my-bucket2" {
   name          = "bkt-demo-002"
-  location      = "us-central1"
+  location      = "asia-south1"
   project = "tt-dev-001"
   force_destroy = true
   public_access_prevention = "enforced"
